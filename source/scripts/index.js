3021268acd7e5b468189b65898c9b8bbfeb7a0f0
Daria Kostenko <dashakstnk@gmail.com>
2024-01-31 21:05:10 +0300

    исправления на допуск

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,17 @@
 /* в этот файл добавляет скрипты*/
+
+// НАВИГАЦИЯ ШАПКИ(mobile burger menu)
+const navButton = document.querySelector('.nav__button');
+const navMain = document.querySelector('.nav');
+
+navMain.classList.remove('nav--no-js');
+navButton.addEventListener('click', () => {
+  if (navMain.classList.contains('nav--open')) {
+    navMain.classList.remove('nav--open');
+    navMain.classList.add('nav--close');
+  } else {
+    navMain.classList.remove('nav--close');
+    navMain.classList.add('nav--open');
+  }
+});
+
